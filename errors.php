c1cc8df5b7fe6bfb08ec04ca037b1930f9ce364e
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-05-24 20:48:51 -0300

    chore: config errors

--- errors.php
+++ errors.php
@@ -1,5 +1,16 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('log_errors', 1);
+ini_set('error_log', '/var/log/myapp');
+
+// PROD
+// error_reporting(E_ALL);
+// ini_set('display_errors', 0);
+// ini_set('log_errors', 1);
+// ini_set('error_log', '/var/log/myapp');
+
 echo $var; // Warning
 
 echo $arr[12]; // Warning
